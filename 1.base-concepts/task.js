ece0ce49117cf423b704ce65c51b052caa74dd9f
Dinara Gaynutdinova <dinarakiss84@gmail.com>
2021-10-05 00:49:48 +0300

    задача 1 решена

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,17 @@
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  "use strict"
+  let d = b ** 2 - 4 * a * c;
+  let x1;
+  if (d < 0) {
+    arr = []
+  } else if (d = 0) {
+    arr.push ( x1 = -b / ( 2 * a ) );
+  } else if (d > 0) {
+    arr.push ( x1 = (-b + Math.sqrt(d) )/(2a));
+    arr.push ( x2 = (-b - Math.sqrt(d) )/(2a));
+  }
   return arr; // array
 }
 
