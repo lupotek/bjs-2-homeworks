6c1cd9c605b749d89af64a85b48909b075b4817c
Dinara Gaynutdinova <dinarakiss84@gmail.com>
2021-10-19 15:19:46 +0300

    Решение задач 1б 2 к уроку 3  - массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,4 @@
-function compareArrays(arr1, arr2) {
-  let result;
+const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
 
-  // Ваш код
+const advancedFilter = (arr) => arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10);
 
-  return result; // boolean
-}
-
-function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
-}
