fb89707ce0cc833a6c92e4616c5a2bd23776715c
Dinara Gaynutdinova <dinarakiss84@gmail.com>
2021-10-25 00:12:37 +0300

    дз обьекты частично решена

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+ this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+this.marks = [mark];
+} else {
+this.marks.push(mark);
+}
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+ this.marks = [mark];
+ } else {
+ this.marks.push(mark);
+ }
+ }
+
+Student.prototype.getAverage = function (mark) {
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i += 1) {
+    sum = sum + this.marks[i];
+  }
+  return avg = sum / this.marks.length;
+}
+  
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
