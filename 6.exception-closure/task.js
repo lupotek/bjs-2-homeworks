ece51cac25333a11f029caca3b210cf7649f67d3
Dinara Gaynutdinova <dinarakiss84@gmail.com>
2021-11-06 18:53:58 +0300

    доработка

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,5 +1,3 @@
-const x = +prompt ("введите цифру")
-
 function parseCount(x) {
   let parsing = Number.parseInt( x );
   if (isNaN (parsing)) {
@@ -15,36 +13,34 @@ function validateCount(x) {
       return err
     }
   }
-  validateCount(x)
 
-  class Triangle {
+class Triangle {
   
-    constructor (a, b, c) {
-      this.a = a;
-      this.b = b;
-      this.c = c;
-      if (a+b < c || b+c < a || a+c < b) {
-    throw new Error ("Треугольник с такими сторонами не существует")
-      }
-    }
-  
-    getPerimeter() {
-    return this.a + this.b + this.c
-    }
-    
-    getArea() {
-      return +Math.sqrt(this.getPerimeter()/2 * (this.getPerimeter()/2 - this.a) * (this.getPerimeter()/2 - this.b) * (this.getPerimeter()/2 - this.c)).toFixed(3)
+  constructor (a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a+b < c || b+c < a || a+c < b) {
+  throw new Error ("Треугольник с такими сторонами не существует")
     }
   }
- 
-  function getTriangle (a, b, c) {
-    try {
-      return new Triangle (a, b, c);
-    } catch (err) {
-      return {getPerimeter = () => “Ошибка! Треугольник не существует”, getArea = () => “Ошибка! Треугольник не существует”}
+
+  getPerimeter() {
+  return this.a + this.b + this.c
+  }
+  
+  getArea() {
+    let halfPerimeter = this.getPerimeter()/2;
+    return +Math.sqrt(halfPerimeter * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c)).toFixed(3)
   }
 }
 
+function getTriangle () {
+    try {
+      return new Triangle (a, b, c); 
+  } catch (err) {
+    return {getPerimeter: () => 'Ошибка! Треугольник не существует!', getArea: () => 'Ошибка! Треугольник не существует!'}
+  }
+}
 
-
- 
+  
