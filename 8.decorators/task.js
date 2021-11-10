0e56c41951ed497c8de88570d1b4871b0dce77f2
Dinara Gaynutdinova <dinarakiss84@gmail.com>
2021-11-10 22:54:13 +0300

    задача 1 и 2(под вопросом)

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,10 +1,43 @@
-function cachingDecoratorNew(func) {
-  // Ваш код
+function cachingDecoratorNew (func) {
+  let cache = [];
+  
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let idx = cache.findIndex((item) => item.hash === args.join(',') );
+    
+    if (idx !== -1 ) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value ;
+      }
+
+      let result = func(...args);
+      cache.push({hash: hash, value: result});
+      
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  } 
+  
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+const sendSignal = () => console.log("Сигнал отправлен");
+
+function debounceDecoratorNew(f, ms) {
+  let flag = false;
+
+  return function (...args) {
+    if (!flag) {
+      f.apply (this, args);
+      flag = true
+      } else {
+      setTimeout(() =>
+        flag = false, ms); 
+    }
+  }
 }
 
 function debounceDecorator2(func) {
